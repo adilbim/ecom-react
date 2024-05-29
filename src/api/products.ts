630c757c4e1cafd0d9cdb6a85d884363b98fda71
Adil Bimzagh <Adil.Bimzagh-ext@mfa.gouv.qc.ca>
2024-05-29 10:51:32 +0100

    Add some error handeling

--- src/api/products.ts
+++ src/api/products.ts
@@ -4,11 +4,21 @@ import type { Product } from "./product.types";
 const ENDPOINT = "/products";
 
 export async function getProducts(): Promise<Product[]> {
-  const { data } = await axios.get<Product[]>(`${ENDPOINT}`);
-  return data;
+  try {
+    const { data } = await axios.get<Product[]>(`${ENDPOINT}`);
+    return data;
+  } catch (error) {
+    console.error("Error fetching products", error);
+    throw error;
+  }
 }
 
 export async function getProductById(id: number): Promise<Product> {
-  const { data } = await axios.get<Product>(`${ENDPOINT}/${id}`);
-  return data;
+  try {
+    const { data } = await axios.get<Product>(`${ENDPOINT}/${id}`);
+    return data;
+  } catch (error) {
+    console.error("Error fetching product by id", error);
+    throw error;
+  }
 }
